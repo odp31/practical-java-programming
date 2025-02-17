1c4f0ca74c8e0a8870069ca1a4343067b94f0e64
Olivia Polos <38380599+odp31@users.noreply.github.com>
2025-02-17 12:02:13 -0500

    Create ThreadSyn1.java

---
+++ ch 3/ThreadSyn1.java
@@ -0,0 +1,31 @@
+// ex. 3.17D Thread Synchronization
+
+class Test {
+  void printMessage(String txt) {
+    for(int i = 1; i <= 5; i++) {
+      System.out.println(txt);
+      try {
+        Thread.sleep(500);
+      } catch(Exception e) {
+        System.out.println(e);}
+    }
+  }
+}
+class ThreadSyn1 extends Thread{
+  String message;
+  Test t;
+  ThreadSyn1(Test t, String message) {
+    this.t = t;
+    this.message = message;
+  }
+  public void run() {
+    t.printMessage(message);
+  }
+  public static void main(String args[]) {
+    Test m = new Test();
+    ThreadSyn1 t1 = new ThreadSyn1(m, "A");
+    ThreadSyn1 t2 = new ThreadSyn1(m, "B");
+    t1.start();
+    t1.start();
+  }
+}
