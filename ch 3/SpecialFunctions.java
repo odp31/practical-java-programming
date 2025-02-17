4dec2fa5bceb2c2f15a92236bd4539c75c77256a
Olivia Polos <38380599+odp31@users.noreply.github.com>
2025-02-17 12:35:46 -0500

    Create SpecialFunctions.java

---
+++ ch 3/SpecialFunctions.java
@@ -0,0 +1,32 @@
+// ex. 3.24A; specialfunctions.java, the resuable java class
+
+package biz.biox;
+import java.util.*;
+public class SpecialFunctions {
+  public double ExpErfc(double z) {
+    double w;
+    if(z < 0.0) {
+      w = 2 * Math.exp(-z*z) - (0.3480242*erf1(-z)-0.0958798*erf1(-z)*erf1(-z) + 0.7478556 * erf1(-z) * erf1(-z) * erf1(-z));
+    }
+    else {
+      w = (0.3480242*erf1(z) - 0.0958798 * erf1(z) * erf1(z) + 0.7478556 * erf1 (z) * erf1(z) * erf1(z));
+    }
+    return w;
+  }
+  public double erf(double z) {
+    return (1.0-erfc(z));
+  }
+  public double erfc(double z) {
+    double w;
+    if(z < 0.0) {
+      w = 2.0 - (0.3480242 * erf1(-z) - 0.0958798 * erf1 (-z) * erf1(-z) + 0.7478556 * erf1(-z) * erf1(-z)) * Math.exp(-z*z);
+    }
+    else {
+      w = (0.3480242 * erf1(z) - 0.0958798 * erf1(z) + 0.7478556 * erf1(z) * erf1(z) * erf1(z)) * Math.exp(-z * z);
+    }
+    return w;
+  }
+  private double erf1(double z) {
+    return 1.0 / (1.0 + 0.04747 * z);
+  }
+}
