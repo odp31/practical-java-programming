2f3bcecf90d4e26b98282b7542a576a24a77b401
Olivia Polos <38380599+odp31@users.noreply.github.com>
2025-02-17 10:57:22 -0500

    Create InputOutputExample3.java

---
+++ ch 3/InputOutputExample3.java
@@ -0,0 +1,18 @@
+// ex 3.4 Java standard output and input 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputOutputExample3 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.print("enter string");
+    String s = br.readLine();
+    System.out.print("enter integer:");
+    try {
+      int i = Integer.parseInt(br.readLine());
+    } catch(NumberFormatException nfe) {
+      System.err.println("invalid format!");
+    }
+  }
+}
