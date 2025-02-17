08c5c301d0f25204034e0b648ce91ad1f7606695
Olivia Polos <38380599+odp31@users.noreply.github.com>
2025-02-17 11:41:17 -0500

    Create HelloThread2.java

---
+++ ch 3/HelloThread2.java
@@ -0,0 +1,21 @@
+// ex. 3.16c multithread programming example
+public class HelloThread2 implements Runnable {
+  String message;
+  HelloThread2 (String message) {
+    this.message = message;
+  }
+  public void run() {
+    System.out.println(message);
+  }
+  public static void main(String[] args) {
+    HelloThread2 ht1 = new HelloThread2("thread 1...");
+    HelloThread2 ht2 = new HelloThread2("thread 2...");
+    HelloThread2 ht3 = new HelloThread2("thread 3...");
+    Thread t1 = new Thread(ht1);
+    Thread t2 = new Thread(ht2);
+    Thread t3 = new Thread(ht3);
+    t1.start();
+    t2.start();
+    t3.start();
+  }
+}
