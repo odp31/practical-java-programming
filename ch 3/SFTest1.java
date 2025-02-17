2fded9cd541f22dc719e3b6ad25a3fd414d3eea4
Olivia Polos <38380599+odp31@users.noreply.github.com>
2025-02-17 12:38:13 -0500

    Create SFTest1.java

---
+++ ch 3/SFTest1.java
@@ -0,0 +1,15 @@
+// ex. 2.24B; SFTest1.java program
+import java.text.*;
+import biz.biox.SpecialFunctions;
+
+class SFTest1 {
+  public static void main(String args[]) {
+    double x; 
+    SpecialFunctions sf = new SpecialFunctions();
+    System.out.println("number \t Erf \t Erfc \t ExpErfc");
+    for(int i = 0; i < 10; i++) {
+      x = (i - 5) / 10.0;
+      System.out.println(x + "\t" + sf.erf(x) + "\t" + sf.erfc(x) + "\t" + sf.ExpErfc(x));
+    }
+  }
+}
