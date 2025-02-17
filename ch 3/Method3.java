34bd3392057c1f75f9176597d675f7d5a57d2131
Olivia Polos <38380599+odp31@users.noreply.github.com>
2025-02-17 11:26:01 -0500

    Create Method3.java

---
+++ ch 3/Method3.java
@@ -0,0 +1,12 @@
+// ex. 3.13 method example
+class Method3 {
+  public static void main(String[] args) {
+    double x = 3, y = 5;
+    double s = add (x, y);
+    System.out.println("sum of x and y is: " + s);
+  }
+  // add() method
+  private static double add(double x, double y) {
+    return x + y;
+  }
+}
