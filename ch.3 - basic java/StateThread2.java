723bf183d30f56f88d481db54c6ba7e15d12c2b2
Olivia Polos <38380599+odp31@users.noreply.github.com>
2025-02-18 08:45:50 -0500

    Create StateThread2.java

---
+++ ch.3 - basic java/StateThread2.java
@@ -0,0 +1,24 @@
+// ex 3.17b; multithread programming example 
+
+public class StateThread2 extends Thread {
+  public void run() {
+    System.out.println("this is a thread...");
+    try{
+      Thread.sleep(5000);
+    } catch(InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+  public static void main(String[] args) {
+    StateThread2 st = new StateThread2();
+    System.out.println("thread before start(): " + st.getState());
+    st.start();
+    System.out.println("thread after start(): " + st.getState());
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    System.out.println("thread after ls:" + st.getState());
+  }
+}
