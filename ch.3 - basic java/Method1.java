4ce7320a533a6fc4414bb5f8f1caf049191b9e0b
Olivia Polos <38380599+odp31@users.noreply.github.com>
2025-02-18 07:44:48 -0500

    Create Method1.java

---
+++ ch.3 - basic java/Method1.java
@@ -0,0 +1,10 @@
+// ex 3.11; using a method
+class Method1 {
+  public static void main(String[] args) {
+    printhello(args[0]);
+  }
+  // printhello method
+  private static void printhello(String msg) {
+    System.out.println("hello " + msg +  "!");
+  }
+}
