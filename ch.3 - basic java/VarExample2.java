129766d73c5504690a1c09887460332a52577a9d
Olivia Polos <38380599+odp31@users.noreply.github.com>
2025-02-18 07:21:42 -0500

    Create VarExample2.java

---
+++ ch.3 - basic java/VarExample2.java
@@ -0,0 +1,13 @@
+//ex 3.1b;java variable example
+public class VarExample2{
+  static int x = 10;
+  static int y = 5;
+  static int z;
+
+  public static void main(String args[]) {
+    System.out.println("x = " + x);
+    System.out.println("y = " + y);
+    z = x + y;
+    System.out.println("x + y =" + z);
+  }
+}
