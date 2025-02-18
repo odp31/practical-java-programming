3bfd9b0163793d2dc4a3c2a8efba8e95a7d5a92b
Olivia Polos <38380599+odp31@users.noreply.github.com>
2025-02-18 07:23:38 -0500

    Create VarExample3.java

---
+++ ch.3 - basic java/VarExample3.java
@@ -0,0 +1,17 @@
+//ex 3.1C; java variable example
+public class VarExample3 {
+  public static void main(String args[]) {
+    short x = 10,y = 5, z= 1;
+    byte B = 17;
+    long w = 2343340000L;
+    double d = 5.01;
+    float f = 5.01f;
+    char ch = 'a';
+    String s = "hello world!";
+    boolean isDone = false;
+
+    System.out.println("ch = " + ch);
+    System.out.println("isDone = " + isDone);
+    System.out.println(s);
+  }
+}
