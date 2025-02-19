73604c74a632c473b025b8de124931fa8b4e1342
Olivia Polos <38380599+odp31@users.noreply.github.com>
2025-02-19 07:44:26 -0500

    Update MainActivity.java

--- ch.6- mobile applications/MainActivity.java
+++ ch.6- mobile applications/MainActivity.java
@@ -1,14 +1,27 @@
-//ex 6.1a; mainactivity.java code
+//ex 6.2a; mainActivity.Java code
 
-package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+  Button b1;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    b1 = (Button)findViewById(R.id.button);
+    b1.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Textview txtView = (TextView) findViewById(R.id.textview);
+        txtView.setText("button has been clicked");
+        txtView.setTextSize(25);
+      }
+    });
   }
 }
